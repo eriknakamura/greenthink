44a7166927dfc6a7936b0c89a594b13db12cf15a
Henry Heffan <henryhef12345@gmail.com>
2017-05-13 17:54:30 -0400

    added air affectors

--- GreenThinkDemo/src/com/erik/GreenThink/Demos/Brain.java
+++ GreenThinkDemo/src/com/erik/GreenThink/Demos/Brain.java
@@ -1,16 +1,111 @@
 package com.erik.GreenThink.Demos;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class Brain
 {
+	public static final double MAX_HUMIDITY=80;
+	public static final double MIN_HUMIDITY=60;
+	
+	public static final double MAX_TEMP=80;
+	public static final double MIN_TEMP_AC=60;
+	
+	public static final double MIN_TEMP_LIGHTS_ON=40;
+	
+	public static final double TIME_LIGHTS_ON_HOURS=3.25;//3:15am
+	public static final double TIME_LIGHTS_OFF_HOURS=12+9.5;//9:30pm
+
+	public static final double TIME_FANS_CYCLE_LEN=3.25;//5 minutes
+	public static final double TIME_FANS_ON_LEN=1;//1 minute
+	
+	
+	
 	AurduinoCommInterface io;
 	Memory mem;
 	public Brain(AurduinoCommInterface io)
 	{
 		mem = new Memory(io);
+		
+		(new WakeCycle(this)).start();
 	}
 	
-	public void wake(double d) {
-		mem.readAll();
+	/*Actuators
+	 * 
+	 * dehumidifyer (
+	 * AC
+	 * lights
+	 * fans
+	 * pH down
+	 * add nutreants
+	 * 
+	 * 
+	 * 
+	 * 
+	 */	
+	public void wake(double dt) {
+		mem.readAll(dt);
+		
+		boolean DHShouldBeOn=humidifyShouldBeOn();
+		boolean ACShouldBeOn=ACShouldBeOn();
+		boolean LightsShouldBeOn=LightsShouldBeOn();
+		boolean FansShouldBeOn=FanShouldBeOn();
+		
+		mem.setDesiredStatesIfNeeded(DHShouldBeOn,ACShouldBeOn,LightsShouldBeOn,FansShouldBeOn);
+	}
+	
+	public boolean humidifyShouldBeOn()
+	{
+		if(mem.humiditySensors.avg()>MAX_HUMIDITY)
+			return true;
+		else if (mem.humiditySensors.avg()<MIN_HUMIDITY)
+			return false;
+		else
+			return mem.getDehumidiferIsOn();
+	}
+	
+	public boolean ACShouldBeOn()
+	{
+		if(mem.humiditySensors.avg()>MAX_TEMP)
+			return true;
+		else if (mem.humiditySensors.avg()<MIN_TEMP_AC)
+			return false;
+		else
+			return mem.getACIsOn();
+	}
+	
+	public boolean LightsShouldBeOn()
+	{
+		Date date = new Date();   // given date
+		Calendar calendar = GregorianCalendar.getInstance(); // creates a new calendar instance
+		calendar.setTime(date);   // assigns calendar to given date 
+		double hour = (double)calendar.get(Calendar.HOUR_OF_DAY)+(double)calendar.get(Calendar.MINUTE)/60.0
+				+(double)calendar.get(Calendar.SECOND)/3600.0; // gets hour in 24h format
+		
+		if(mem.tempSensors.avg()<MIN_TEMP_LIGHTS_ON)
+			return true;
+		else if (TIME_LIGHTS_ON_HOURS<hour && hour<TIME_LIGHTS_OFF_HOURS)
+			return true;
+		else
+			return false;
+	}
+	
+	public boolean FanShouldBeOn()
+	{
+		Date date = new Date();   // given date
+		Calendar calendar = GregorianCalendar.getInstance(); // creates a new calendar instance
+		calendar.setTime(date);   // assigns calendar to given date 
+		double minute = (double)calendar.get(Calendar.HOUR_OF_DAY)*60.0
+				+(double)calendar.get(Calendar.MINUTE)+(double)calendar.get(Calendar.MINUTE)
+				+(double)calendar.get(Calendar.SECOND)/60.0;
+		
+		if(mem.getDehumidiferIsOn()||mem.getACIsOn())
+			return true;
+		if(minute%TIME_FANS_CYCLE_LEN <TIME_FANS_ON_LEN)
+			return true;
+		else
+			return false;
 	}
 	
 	
--- GreenThinkDemo/src/com/erik/GreenThink/Demos/Memory.java
+++ GreenThinkDemo/src/com/erik/GreenThink/Demos/Memory.java
@@ -1,6 +1,12 @@
 package com.erik.GreenThink.Demos;
 
 public class Memory {
+	public static final String DEHUMIDIFYER_NAME="";
+	public static final String AC_NAME="";
+	public static final String LIGHTS_NAME="";
+	public static final String FANS_NAME="";
+	
+	
 	AurduinoCommInterface io;
 	
 	SensorMemory tempSensors;
@@ -13,20 +19,57 @@ public class Memory {
 		this.io=io;
 	}
 
-	public void readAll(double deltaT)
+	public void readAll(double dt)
 	{
 		// TODO Auto-generated method stub
 		
 	}
+
+	public boolean getDehumidiferIsOn() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean getACIsOn() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
-	/*Actuators
-	 * 
-	 * dehumidifyer
-	 * AC
-	 * lights
-	 * fans
-	 * pH down
-	 * add nutreants
-	 * 
-	 */
+	public boolean getLightsIsOn() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean getFansIsOn() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void setDesiredStatesIfNeeded(boolean DH,
+			boolean AC, boolean lights,
+			boolean fans) {
+		if(DH&&!getDehumidiferIsOn())
+			io.setState(DEHUMIDIFYER_NAME, true);
+		else if(!DH&&getDehumidiferIsOn())
+			io.setState(DEHUMIDIFYER_NAME, false);
+		
+		if(AC&&!getACIsOn())
+			io.setState(AC_NAME, true);
+		else if(!AC&&getACIsOn())
+			io.setState(AC_NAME, false);
+
+		if(lights&&!getLightsIsOn())
+			io.setState(LIGHTS_NAME, true);
+		else if(!lights&&getLightsIsOn())
+			io.setState(LIGHTS_NAME, false);
+
+		if(fans&&!getFansIsOn())
+			io.setState(FANS_NAME, true);
+		else if(!fans&&getFansIsOn())
+			io.setState(FANS_NAME, false);
+		
+	}
+
+	
+	
 }
